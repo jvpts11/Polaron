b61d782100766bcd22fb754b1db96150741c8eba
jvpts11 <jvptsyt11@gmail.com>
2026-07-27 19:38:15 +0100

    Type each binary operand once so deep synthesized hashes don't hang codegen

--- installer/build-msi.ps1
+++ installer/build-msi.ps1
@@ -5,7 +5,7 @@
 #   ./build-msi.ps1                       # -> installer/dist/LDP3-1.0.4.msi
 param(
     [string]$Config = "Release",
-    [string]$Version = "1.0.12"
+    [string]$Version = "1.0.13"
 )
 $ErrorActionPreference = "Stop"
 $here = $PSScriptRoot
